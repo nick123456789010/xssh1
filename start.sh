cf1aed330480d1244ade0b58e2fd7be0ec4c688e
nick123456789010 <137611297+nick123456789010@users.noreply.github.com>
2024-05-02 17:14:41 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ source /app/config.sh
 service ssh start
 service nginx start
 
-echo "set ngrok token: $NGROK_TOKEN"
+echo "set ngrok token:2VTiAk0b9Pk0fH624feu1eCfpGX_4sBzXG29uaxnuNdRmz6gU"
 ngrok authtoken $NGROK_TOKEN
 echo "start ngrok service"
 ngrok tcp 22 --log=stdout > ngrok.log
